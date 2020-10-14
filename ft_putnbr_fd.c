f458fceb70b4356e757ebbc2274b92d3e4da9ef7
yassin slati <sltyassin@gmail.com>
2020-10-14 14:03:07 +0100

    libft

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yslati <yslati@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/20 11:27:44 by yslati            #+#    #+#             */
+/*   Updated: 2019/10/27 12:29:04 by yslati           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void		ft_putnbr_fd(int n, int fd)
+{
+	unsigned int nb;
+
+	nb = n;
+	if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		nb = (-1) * nb;
+	}
+	if (nb > 9)
+	{
+		ft_putnbr_fd(nb / 10, fd);
+		ft_putnbr_fd(nb % 10, fd);
+	}
+	else
+		ft_putchar_fd('0' + nb, fd);
+}
